021d215718f0a0a08925ad2a8adb82d3b4118cc5
SUMYAK JAIN <3456gaurav@gmail.com>
2020-05-22 02:08:45 +0530

    Update latest.py

--- task 1/latest.py
+++ task 1/latest.py
@@ -1,1 +1,7 @@
-
+import pandas as pd
+data= pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/05-20-2020.csv')
+data['Active'] = data['Confirmed'] - data['Deaths'] - data['Recovered']
+final = data.groupby('Country_Region')['Confirmed', 'Deaths', 'Recovered', 'Active'].sum().reset_index()
+print(final)
+a=input("Enter a country/region:")
+print(final.loc[(final['Country_Region'] == a)])
