db076f9b4d4f8e3d31e553bd73368806f2f2bfd3
SUMYAK JAIN <3456gaurav@gmail.com>
2020-05-26 03:45:49 +0530

    Update plotly.py

--- task 6/plotly.py
+++ task 6/plotly.py
@@ -2,10 +2,11 @@ import plotly.graph_objects as go
 import pandas as pd
 import pycountry
 
+#latest date dataset
 URL_DATASET = r'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/05-24-2020.csv'
 df = pd.read_csv(URL_DATASET)
 
-df['Active'] = df['Confirmed'] - df['Deaths'] - df['Recovered']
+df['Active'] = df['Confirmed'] - df['Deaths'] - df['Recovered'] #genrating active cases
 df1 = df.groupby('Country_Region')['Confirmed', 'Deaths', 'Recovered', 'Active'].sum().reset_index()
 
 list_countries = df1['Country_Region'].unique().tolist()
@@ -27,11 +28,13 @@ for k, v in d_country_code.items():
 for col in df1.columns:
     df1[col] = df1[col].astype(str)
 
-df1['text'] = df1['Country_Region'] + '<br>' + \
+ # displaying the details
+df1['text'] = df1['Country_Region'] + '<br>' + \  
     'Confirmed: ' + df1['Confirmed'] +'<br>'\
     'Recovered: ' + df1['Recovered'] + '<br>' + \
     'Deaths: ' +df1['Deaths'] + '<br>' + \
     'Active: ' +df1['Active']
+
 
 fig = go.Figure(data=go.Choropleth(
     locations = df1['iso_alpha'],
